50cddd73e75655cee053ebf0ede4182aa9279d2c
imonaar <kevinmonari22@gmail.com>
2022-02-12 12:35:45 +0300

    ally

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,21 @@
 const form = document.querySelector("#form");
-const menu = document.querySelector("#menu");
-const jsNav = document.querySelector(".js-nav");
+const toggle = document.querySelector("#toggle");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".js-nav");
 
 
-menu.addEventListener('click', ()=>{
-  menu.classList.toggle('open')
-  jsNav.classList.toggle('open')
+toggle.addEventListener('click', ()=>{
+  toggle.classList.toggle('open')
+  nav.classList.toggle('open')
+
+  if( nav.classList.contains("open")){
+    toggle.setAttribute('aria-expanded' , "true")
+  } else{
+    toggle.setAttribute("aria-expanded", "false");
+  }
 })
+
+// https://www.a11ymatters.com/pattern/mobile-nav/
 
 if (form) {
   form.addEventListener("submit", (e) => {
